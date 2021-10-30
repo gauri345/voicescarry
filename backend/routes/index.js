5d6b1f0028fa8963291d0ba7c2bb86a86de7130f
Gauri Upreti <gauri.upreti1@gmail.com>
2021-10-30 13:50:58 +0200

    adds server documentation

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'TP-WorkerWellbeing' });
 });
 
+router.get('/all_users', function (req, res, next) {
+
+});
+
 module.exports = router;
