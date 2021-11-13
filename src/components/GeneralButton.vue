b7c301d6a747eba5b76f0945cc39327c5d8a7ab9
falbrec1 <84471566+falbrec1@users.noreply.github.com>
2021-11-13 06:54:46 +0100

    Basic information of the button

--- src/components/GeneralButton.vue
+++ src/components/GeneralButton.vue
@@ -1,14 +1,20 @@
 <template>
-    <button>
-        Enter the page
-    </button>
+  <button @click="onClick()" :style="{ background: color }" class="btn">
+    {{ text }}
+  </button>
 </template>
 
 <script>
-
+export default {
+  name: 'Button',
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    onClick() {
+      this.$emit('btn-click')
+    },
+  },
+}
 </script>
-
-
-<style>
-
-</style>
