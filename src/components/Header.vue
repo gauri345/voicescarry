58897154dc0bf3323e7073bc3ad4fa24752a407d
Amnney <37539813+mischhh@users.noreply.github.com>
2021-11-14 10:17:48 +0100

    Header init

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <nav class="navbar navbar-dark bg-primary">
+        <a class="navbar-brand" href="#">
+        WorkerWellbeing
+        </a>
+    </nav>
+  </header>
+</template>
+
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+
+<style>
+
+</style>
