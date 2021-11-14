af614d25321c715e43c43256b2e49300107abd21
nildeniiz <nuengoer@mail.uni-mannheim.de>
2021-11-14 19:08:18 +0100

    Initial Changes
    Header Footer added

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -1,15 +1,34 @@
 <template>
+
+  <div>
+    
+    <!-- The Header bar goes here -->
+    <div class="header">
+      <Header text="Welcome! You’ve successfully entered this survey."></Header>
+    </div>
+    <div class="footer">
+      <Footer text="Random Footer Text"></Footer>
+    </div>
+
+
+  </div>
   <router-link to="/question">
-    <GeneralButton text="Start Questionnaire"></GeneralButton>
+    <CustomButton text="Start a Questionnaire"></CustomButton>
   </router-link>
 </template>
 
 <script>
-  import GeneralButton from "@/components/GeneralButton";
+  import CustomButton from "@/components/CustomButton";
+  import Header from "@components/Header";
+  import Footer from "components/Footer";
   
   export default {
     name: "Homepage",
-    components: { GeneralButton }
+    components: {CustomButton, 
+    Header, 
+    Footer
+    }
+
   }
 </script>
 
