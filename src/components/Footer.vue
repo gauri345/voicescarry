402678eb08b4239e4155718940cd4a89ab60acf3
mischhh <37539813+mischhh@users.noreply.github.com>
2021-11-14 17:53:39 +0100

    FooterRouting

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,8 @@
 <template>
     <nav class="navbar justify-content-center navbar-expand fixed-bottom navbar-dark bg-primary">
             <div class="navbar-nav">
-                    <a class="nav-item nav-link active" href="/Homepage">Home <span class="sr-only">(current)</span></a>
-                    <a class="nav-item nav-link" href="/question">Survey</a>
+                    <a class="nav-item nav-link"><router-link to="/">Home</router-link></a>
+                    <a class="nav-item nav-link"><router-link to="/question">Survey</router-link></a>
                     <a class="nav-item nav-link" href="#">Complaint</a>
                     <a class="nav-item nav-link" href="#">Language</a>
             </div>
@@ -20,6 +20,14 @@ export default {
 nav {
     background: radial-gradient(122.27% 198.92% at -22.27% -27.38%, #0070BA 0%, #1546A0 100%);
     margin-top: 10px;
+}
+a:visited {
+  color:rgba(255, 255, 255, 0.75);
+  text-decoration: none;
+}
+a:link {
+  color:rgba(255, 255, 255, 0.75);
+  text-decoration: none;
 }
 
 </style>
