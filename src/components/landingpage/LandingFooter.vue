3175300eb53f634e337dcc167ac9165ef0e5bd9c
mischhh <37539813+mischhh@users.noreply.github.com>
2021-11-16 18:59:10 +0100

    AddedLogos
    Logos added for footer and browser bar

--- src/components/landingpage/LandingFooter.vue
+++ src/components/landingpage/LandingFooter.vue
@@ -1,8 +1,9 @@
 <template>
     <nav class="navbar fixed-bottom navbar-dark bg-primary">
-        <a class="navbar-brand" href="#">
-        Test
-        </a>
+        <div class="picture-wrapper" >
+          <img alt="Uni Mannheim" src="../../assets/Logo_UMA_EN_Weiss.png" style="float: left"/>
+          <img alt="Vaude" src="../../assets/vaude-logo.jpg" style="float: right"/>
+        </div>
     </nav>
 </template>
 
@@ -16,5 +17,10 @@ export default {
 nav {
 background: radial-gradient(122.27% 198.92% at -22.27% -27.38%, #0070BA 0%, #1546A0 100%);
 }
-
+img {
+  height: 50px;
+}
+.picture-wrapper{
+  width:100%;
+}
 </style>
