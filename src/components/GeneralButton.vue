dc537cd992f8037705f32239cb53e909cf618aa1
falbrec1 <84471566+falbrec1@users.noreply.github.com>
2021-12-20 16:48:46 +0100

    Entered an info page before the survey + alignment of GeneralButton

--- src/components/GeneralButton.vue
+++ src/components/GeneralButton.vue
@@ -28,23 +28,24 @@ export default {
 .btn {
   cursor: pointer;
   display: inline-block;
-  vertical-align: top;
+  vertical-align: middle;
   position: relative;
   color: rgb(255, 255, 255);
   font-size: 16px;
   font-weight: bold;
   border: 0;
-  padding: 10px;
   width: 100%;
   height: fit-content;
   background: radial-gradient(122.27% 198.92% at -22.27% -27.38%, #0070BA 0%, #1546A0 100%);
   box-shadow: 0px 24px 48px -18px rgba(21, 70, 160, 0.5);
   border-radius: 20px;
   text-align:center;
-  margin-top: 10px;
+  padding-top: 10px;
+  padding-bottom: 5px;
 }
 .button-text-wrapper{
   display: inline-flex;
+  text-align:center;
 }
 .material-icons.icon{
   display: flex;
---
+++ src/components/question/EntryPage.vue
@@ -0,0 +1,78 @@
+<template>
+    <Header HeaderIcon="spa" HeaderText="Wellbeing at Work"/>
+    <div id="body">
+    <span  v-if="'en' === $i18n.locale" class="material-icons audio" v-on:click="readQuestionContent">volume_up</span>
+    <div class="text">
+    <p> {{ $t('welcome_survey') }} </p>
+    </div>
+    <div class="team-picture">
+        <img id="Team" alt="Team" src="../../assets/TeamPicture.jpg"/>
+    </div>
+    <div class="text">
+        <p> {{ $t('explain_survey') }} </p>
+    </div>
+    <div class=buttonwrapper>
+        <router-link to="/question/1">
+        <GeneralButton id="startbutton" text="Start the Questionnaire!"/>
+        </router-link>
+    </div>
+    </div>
+    <Footer/>
+</template>
+
+<script>
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import GeneralButton from "@/components/complaint/ComplaintButton"
+import {textReader} from "@/util/Speech";
+
+export default {
+  name: 'EntryPage',
+  components: {
+      Header,
+      Footer,
+      GeneralButton
+      },
+data() {
+    return {
+      isReading: false
+    }
+  },
+  methods: {
+    readQuestionContent: function () {
+        const textToRead =
+            this.$i18n.t('welcome_survey').replace('!', '. ') +
+            this.$i18n.t('explain_survey').replace('!', '. ');
+
+        console.log(this.$i18n.locale);
+
+        textReader(textToRead);
+
+        }
+  }
+}
+</script>
+
+<style scoped>
+img {
+    width: 80%;
+    margin-bottom: 15px;
+}
+#startbutton {
+    width: 50%;
+}
+.text {
+    font-size: 20px;
+    margin-left: 10%;
+    margin-right: 10%;
+}
+#body {
+    padding-bottom: 80px;
+}
+.material-icons.audio {
+  font-size: 32px;
+  cursor: pointer;
+  color: #2c3e50;
+  margin-bottom: 15px;
+}
+</style>
