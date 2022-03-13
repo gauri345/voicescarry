9323ffe64d0921570796592a927dfb649f2f45cd
Gauri Upreti <gauri.upreti1@gmail.com>
2022-03-13 11:26:54 +0100

    restructure

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,27 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const QuestionController = require('../controller/QuestionController');
+const SurveyController = require('../controller/SurveyController');
+const FactoryController = require('../controller/FactoryController');
+const AnswerController = require('../controller/AnswerController');
 
+router.route('/question')
+    .get(QuestionController.index)
+    .post(QuestionController.post);
 
-router.get('/todo', (req, res) => {
-    const todo = [
-        {
-            "id": 1,
-            "todo": "Buy chicken"
-        },
-        {
-            "id": 2,
-            "todo": "Cook chicken"
-        },
-        {
-            "id": 3,
-            "todo": "Eat chicken"
-        }
-    ];
+router.route('/question/:number')
+    .delete(QuestionController.delete);
 
+router.route('/factory')
+    .get(FactoryController.index)
+    .post(FactoryController.post);
 
-    return res.send(todo);
+router.route('/factory/exists')
+    .get(FactoryController.checkIfExists);
 
-});
+router.route('/survey/start/:factoryCode')
+    .get(SurveyController.createSurvey)
 
+router.route('/answer/storeBulk/')
+    .get(AnswerController.index)
+    .post(AnswerController.insertBulk)
 
 module.exports = router;
