20a9f14db4c0abedcc85547074e47a312452afda
Gauri Upreti <gauri.upreti1@gmail.com>
2022-03-14 09:45:03 +0100

    CLV-40:[chore] adds vuex

--- dashboard/src/main.js
+++ dashboard/src/main.js
@@ -1,10 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import store from './store'
 
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/style.css';
 import router from "./routes";
 
+createApp(App).use(router).use(store).mount('#app')
 
-createApp(App).use(router).mount('#app')
