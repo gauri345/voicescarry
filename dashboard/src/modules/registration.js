04d49853e38b0557dd6a229ab95e69e914c52f47
Gauri Upreti <gauri.upreti1@gmail.com>
2022-03-19 18:24:12 +0100

    CLV-57:[feat] comapres hash requestd by user and hash store in the database

--- dashboard/src/modules/registration.js
+++ dashboard/src/modules/registration.js
@@ -1,34 +1,5 @@
 const axios = require('axios');
 import router from '../routes';
-
-
-//function test() {
-
-    //let fun1 = (aNumber) => {
-        //console.log('function one', aNumber);
-   // }
-
-    //let fun2 = (kk) => {
-       // console.log('function two', kk());
-    //}
-
-   // fun2(function (){});
-
-    //let fun3 = (callback) => {
-    //    console.log('kurkur ko kando');
-     //   callback(10);
-       // console.log('biraloko ko turi');
-    //}
-
-   // const kando = function (anotherNumber) { console.log('a number: ', anotherNumber); };
-   // fun3(kando);
-
-   // fun3(function (anotherNumber) { console.log('a number: ', anotherNumber); });
-    // kukur ko kando
-    // a number: 10
-    // biralako ko turi
-
-
 export default {
     state: {
         errorMessage: ''
