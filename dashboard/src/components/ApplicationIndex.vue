db319b325b354603b2e507cce8340ed164068d1c
Gauri Upreti <gauri.upreti1@gmail.com>
2022-05-08 19:58:00 +0200

    NO-TICKET: [feat] removes header and sidebar from login page

--- dashboard/src/components/ApplicationIndex.vue
+++ dashboard/src/components/ApplicationIndex.vue
@@ -1,21 +1,12 @@
 <template>
-  <HeaderMenu />
-  <div class="container-fluid">
-    <div class="row">
-      <SidebarMenu />
-      <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-        <router-view></router-view>
-      </main>
-    </div>
-  </div>
+  <router-view></router-view>
 </template>
 
 <script>
-import HeaderMenu from "@/components/home/HeaderMenu";
-import SidebarMenu from "@/components/home/SidebarMenu";
+
 export default {
   name: "ApplicationIndex",
-  components: {SidebarMenu, HeaderMenu}
+  components: {}
 }
 </script>
 
--- dashboard/src/components/home/DashboardHome.vue
+++ dashboard/src/components/home/DashboardHome.vue
@@ -1,23 +1,33 @@
 <template>
-  <WelcomeComponent/>
-  <div class="row">
-    <div class="col-sm-6">
-      <router-view></router-view>
-    </div>
-    <div class="col-sm-6">
-      <ChartsIndex/>
+  <HeaderMenu />
+  <div class="container-fluid">
+    <div class="row">
+      <SidebarMenu />
+      <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+        <WelcomeComponent/>
+        <div class="row">
+          <div class="col-sm-6">
+            <router-view></router-view>
+          </div>
+          <div class="col-sm-6">
+            <ChartsIndex/>
+          </div>
+        </div>
+      </main>
     </div>
   </div>
 </template>
 
 <script>
 
+import SidebarMenu from "@/components/home/SidebarMenu";
 import WelcomeComponent from "@/components/home/WelcomeComponent";
 import ChartsIndex from "@/components/charts/ChartsIndex";
+import HeaderMenu from "@/components/home/HeaderMenu";
 
 export default {
   name: "DashboardHome",
-  components: {ChartsIndex, WelcomeComponent},
+  components: {ChartsIndex, WelcomeComponent, SidebarMenu, HeaderMenu},
 }
 </script>
 
