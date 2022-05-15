6c23a9aecb6374781396fb0c32e94d74f26ff0bb
Gauri Upreti <gauri.upreti1@gmail.com>
2022-05-15 19:36:31 +0200

    [CLV-73]:delete confirmation for feedback

--- dashboard/src/components/feedback/FeedbacksComponent.vue
+++ dashboard/src/components/feedback/FeedbacksComponent.vue
@@ -2,15 +2,15 @@
   <component-header text="Feedbacks"/>
   <div v-if="feedbackDeleteInfo" class="alert alert-success" role="alert">
     Feedback successfully deleted
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    <button aria-label="Close" class="btn-close" data-bs-dismiss="alert" type="button"></button>
   </div>
   <div v-if="feedbackDeleteError" class="alert alert-danger" role="alert">
     Failed deleting the feedback.
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    <button aria-label="Close" class="btn-close" data-bs-dismiss="alert" type="button"></button>
   </div>
   <div v-if="serverErrorDisplayed" class="alert alert-danger" role="alert">
     Problem connecting to server. Please try again.
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    <button aria-label="Close" class="btn-close" data-bs-dismiss="alert" type="button"></button>
   </div>
   <table class="table bg-dark text-info text-lg-start">
     <thead class="table-bordered">
@@ -27,10 +27,26 @@
       <td>{{ feedback.surveyCode }}</td>
       <td class="roles-td">{{ feedback.content }}</td>
       <td>
-        <a href="javascript:void(0);">
-          <span class="material-icons-outlined text-danger" title="Delete Feedback"
-                @click="handleDeleteFeedback(feedback._id)">delete</span>
+        <a :data-bs-target="`#deleteFeedback_${feedback._id}`" data-bs-toggle="modal" href="javascript:void(0);">
+          <span class="material-icons-outlined text-danger" title="Delete Feedback">delete</span>
         </a>
+        <!-- Modal -->
+        <div :id="`deleteFeedback_${feedback._id}`" aria-hidden="true" class="modal fade" tabindex="-1">
+          <div class="modal-dialog">
+            <div class="modal-content">
+
+              <div class="modal-body text-danger">
+                Are you sure you want to delete?
+              </div>
+              <div class="modal-footer">
+                <button class="btn btn-danger" data-bs-dismiss="modal" type="button"
+                @click="handleDeleteFeedback(feedback._id)">Yes
+                </button>
+                <button class="btn btn-secondary" data-bs-dismiss="modal" type="button">No</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </td>
     </tr>
     </tbody>
@@ -49,9 +65,9 @@ export default {
   methods: {
     ...mapActions(['fetchFeedback', 'deleteFeedback']),
     handleDeleteFeedback(feedbackId) {
-      this.deleteFeedback(feedbackId)
-      console.log(feedbackId);
-    }
+      this.deleteFeedback(feedbackId);
+      return true;
+    },
   },
   mounted() {
     this.fetchFeedback();
