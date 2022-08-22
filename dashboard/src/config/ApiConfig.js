dd50b73cdc3a3d159b2ea2d4a405fb2773f4db7f
Gauri Upreti <gauri.upreti1@gmail.com>
2022-08-22 20:12:00 +0200

    triex fixing nginx

--- dashboard/src/config/ApiConfig.js
+++ dashboard/src/config/ApiConfig.js
@@ -1,3 +1,7 @@
 export default {
-    API_BASE_URL: "http://localhost:4000/api"
+    // Use this when local
+    // API_BASE_URL: "http://localhost:4000/api"
+
+    // use this when production
+    API_BASE_URL: "https://voicescarry.uni-mannheim.de/api/language"
 }
